f579e093db2fe16ec8c5cc9802e1556e2463d6dc
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-08-23 13:10:20 -0300

    first commit

---
+++ vibi-client/app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>Aiuda c2</h1>
+      </main>
+    )
+  }
+  
--- vibi-client/components/navbar.tsx
+++ vibi-client/components/navbar.tsx
@@ -4,7 +4,7 @@ export default function NavBar (){
     return(
         <>
         <nav>
-
+        <h1>dsads</h1>
         </nav>
         </>
     )
