41495300d3a74b7caf7a080ed35ff574e83b636c
santilagos <sntlgs@gmail.com>
2023-08-24 11:45:03 -0300

    add propertyDetail model

--- vibi-server/src/config/env.config.ts
+++ vibi-server/src/config/env.config.ts
@@ -2,7 +2,7 @@ const env = {
 	port: process.env.PORT || 3000,
     databaseUrl: process.env.DATABASE_URL,
     secretKey: process.env.SECRET_KEY || 'secret',
-
+    saltKey: process.env.salt,
     stipeSecretKey: process.env.STRIPE_SECRET_KEY
 
 }
