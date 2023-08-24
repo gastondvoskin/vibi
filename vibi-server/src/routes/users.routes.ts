e276adc74ebddad300e128628d3c62291070402c
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-08-24 09:25:57 -0300

    update

--- vibi-server/src/routes/users.routes.ts
+++ vibi-server/src/routes/users.routes.ts
@@ -7,14 +7,4 @@ const userController = new UserController();
 
 userRouter.get('/all', userController.getUsers);
 
-userRouter.get('/:id', userController.getUserById);
-
-userRouter.post('/register', userController.registerUser);
-
-userRouter.post('/login', userController.loginUser);
-
-userRouter.put('/:id',authMiddleware, userController.updateUser);
-
-userRouter.delete('/:id',authMiddleware, userController.deleteUser);
-
 export default userRouter;
