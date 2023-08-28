1a14c300e4affbac572cbb96eb43e46cb0fdc2c0
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-08-28 11:09:27 -0300

    pre

--- vibi-client/app/layout.tsx
+++ vibi-client/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavBar from '../components/NavBar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
         {children}
       
       </body>
