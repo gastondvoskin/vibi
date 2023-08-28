f5e1dfcfdb47df64dc12559f883c19a26cf1c290
santilagos <sntlgs@gmail.com>
2023-08-28 13:48:46 -0300

    add createPublication Controller

--- vibi-server/src/controllers/publication.controllers.ts
+++ vibi-server/src/controllers/publication.controllers.ts
@@ -4,19 +4,77 @@ import { Request, Response } from 'express';
 export class PublicationController {
   async getPublications(req: Request, res: Response) {
     try {
-      const publications = await prisma.publication.findMany()
+      const publications = await prisma.publication.findMany({include:{property:{include:{propertyAddress:true,propertyDetail:true,propertyInformation:true}}}})
       res.status(200).json(publications);
     } catch (error) {
       res.status(500).json({ error: 'Something went wrong.' });
     }
   }
 
-  async registerProperty(req: Request, res: Response) {
+  async postPublication(req: Request, res: Response) {
     try {
+      const { publication, property, propertyAddress, propertyInformation, propertyDetail } = req.body;
+      const {userId} = req.params
       
+      const newPropertyInformation = await prisma.propertyInformation.create({
+        data: {
+          ...propertyInformation,
+        },
+      });
+  
+      const newPropertyDetail = await prisma.propertyDetail.create({
+        data: {
+          ...propertyDetail,
+        },
+      });
+
+      const newPropertyAddress = await prisma.propertyAddress.create({
+        data: {
+          ...propertyAddress,
+        },
+      });
+  
+      const newProperty = await prisma.property.create({
+        data: {
+          ...property,
+          propertyInformation: {
+            connect: {
+              propertyInformation_Id: newPropertyInformation.propertyInformation_Id,
+            },
+          },
+          propertyDetail: {
+            connect: {
+              propertyDetail_Id: newPropertyDetail.propertyDetail_Id,
+            },
+          },
+          propertyAddress: {
+            connect: {
+              propertyAddress_Id: newPropertyAddress.propertyAddress_Id,
+            },
+          },
+        },
+      });
+  
+      const newPublication = await prisma.publication.create({
+        data: {
+          ...publication,
+          property: {
+            connect: {
+              property_Id: newProperty.property_Id,
+            },
+          },
+          user: {
+            connect: {
+              user_Id: userId,
+            },
+          },
+        },
+      });
+  
+      res.status(200).json(newPublication);
     } catch (error) {
-      
+      res.status(500).json({ error: 'Something went wrong.' });
     }
   }
-
+  
 }
--- vibi-server/src/controllers/user.controllers.ts
+++ vibi-server/src/controllers/user.controllers.ts
@@ -1,14 +1,11 @@
-import generateToken from "@/auth/generateToken";
-import env from "@/config/env.config";
 import prisma from "@/config/prisma-client.config";
-import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 
 export class UserController {
   async getUsers(req: Request, res: Response) {
     try {
       const users = await prisma.user.findMany();
-      res.status(200).json(users);
+      res.status(200).send(users);
     } catch (error) {
       res.status(500).json({ error: "Something went wrong." });
     }
@@ -16,14 +13,13 @@ export class UserController {
 
   async registerUser(req: Request, res: Response) {
     try {
-      /* const id = req.body;
-      const user = await prisma.user.findUnique({
-        where: { user_id: id },
-        include: { property: { include: { propertyInformation: true } } },
-      }); */
-      res.status(200).json("Estoy funcionando");
+      const data = req.body
+      const newUser = await prisma.user.create({
+        data
+      })
+      res.status(200).json(newUser);
     } catch (error) {
-      res.status(400).json("No Estoy funcionando");
+      res.status(500).json({ error: "Something went wrong." });
     }
   }
 }
