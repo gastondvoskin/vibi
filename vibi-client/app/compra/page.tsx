c6526b9a8898ea2342284217e2cfe45aaecc6d26
gastondvoskin <dvoskingaston@gmail.com>
2023-08-29 12:53:25 -0300

    Added SearchBar and SelectFilter

--- vibi-client/app/compra/page.tsx
+++ vibi-client/app/compra/page.tsx
@@ -1,9 +1,11 @@
+import Filters from "../../components/Filters"
 import CardsContainer from "../../components/CardsContainer"
 
 export default function Compra() {
   return (
     <main>
       <h1>Lista de propiedades</h1>
+      <Filters />
       <CardsContainer />
     </main>
   )
