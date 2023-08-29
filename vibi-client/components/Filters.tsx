e3de9fb83c4da356088cd3ae7107d04cb6ae99d2
gastondvoskin <dvoskingaston@gmail.com>
2023-08-29 12:56:37 -0300

    Added SearchBar and SelectFilter

--- vibi-client/components/Filters.tsx
+++ vibi-client/components/Filters.tsx
@@ -3,21 +3,10 @@ import SearchBar from "./SearchBar";
 import SelectFilter from "./SelectFilter";
 
 const Filters = () => {
-  const handleChange = () => {
-    alert("dispara filtrar por tipo de vivienda");
-  };
   return (
     <div>
       <SearchBar />
       <SelectFilter />
-      
-      {/* <select name="vivienda" id="vivienda" onChange={() => handleChange()}>
-        <option value="" disabled>
-          Tipo de vivienda
-        </option>
-        <option value="casa">Casa</option>
-        <option value="departamento">Departamento</option>
-      </select> */}
     </div>
   );
 };
