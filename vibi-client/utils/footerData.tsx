ed9c637ed7687cebcf4eaf019ea848d298985b14
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-08-31 06:44:52 -0300

    fix(footer): route added

--- vibi-client/utils/footerData.tsx
+++ vibi-client/utils/footerData.tsx
@@ -1,2 +1,2 @@
 
- export const pathnames:string[] = ["/home","/servicios", "/financia", "/vende", "/compra", "/compra/detalle" ]
+ export const pathnames:string[] = ["/home","/servicios", "/financia", "/vende", "/compra", "/compra/detalle", "/vende/crear-anuncio" ]
