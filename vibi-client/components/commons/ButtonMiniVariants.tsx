7c3303d59b5d72a5174ffdfd47aab9d23a6f1465
EusebioLujan <narboleta@gmail.com>
2023-09-01 13:36:19 -0300

    feature(view-recomendado):view full v.1

---
+++ vibi-client/components/commons/ButtonMiniVariants.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react"
+
+interface Props {
+    text : string,
+    leftIcon?: ReactNode,
+    rightIcon?: ReactNode,
+    border: boolean,
+    bgColor: string,
+    brColor?: string,
+    txColor: string
+}
+export default function ButtonMiniVariants({text, leftIcon, rightIcon, bgColor, txColor, border, brColor}:Props){
+    return(
+        <>
+        <button className={ border ? `border-2 h-9 ${brColor} ${bgColor}  rounded-[4px]` : ` ${bgColor}  rounded-[4px]`}>
+            <section className={`${txColor} ` + "text-[24px]"}>
+                {leftIcon}
+            </section>
+            <span className={`mx-2 px-6 py-3 ${txColor} `} >{text}</span>
+            <section  className={`${txColor} ` + "text-[24px]"}>
+                {rightIcon}
+            </section>
+        </button>
+        </>
+    )
+}
