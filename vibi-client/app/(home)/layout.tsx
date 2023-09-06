0c7e6905861a4431f27f54e971f0ec5cc48cdd1b
Carlos <andre021297@gmail.com>
2023-09-05 23:03:36 -0500

    'feature:home-page-finished'

--- vibi-client/app/(home)/layout.tsx
+++ vibi-client/app/(home)/layout.tsx
@@ -1,8 +1,5 @@
-"use client";
 import Footer from "../../components/home/Footer";
 import NavBar from "../../components/home/NavBar";
-import { Provider } from "react-redux";
-import store from "../../redux/store";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -10,14 +7,11 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <Provider store={store}>
-      <html lang="en">
-        <body className={"font-normal max-w-[1280px] flex flex-col justify-center m-auto text-[#102C57]"}>
-          <NavBar />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </Provider>
+
+    <div className={"font-normal max-w-[1280px] flex flex-col justify-center m-auto bg-white text-[#102C57]"}>
+      <NavBar />
+      {children}
+      <Footer />
+    </div>
   );
 }
