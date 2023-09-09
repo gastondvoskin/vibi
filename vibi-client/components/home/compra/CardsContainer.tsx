060f87f0c289039a79963c4eacd83216683bccf0
gastondvoskin <dvoskingaston@gmail.com>
2023-09-09 11:46:42 -0300

    Updated Card and CardsContainer to new look and feel

--- vibi-client/components/home/compra/CardsContainer.tsx
+++ vibi-client/components/home/compra/CardsContainer.tsx
@@ -1,20 +1,29 @@
 import Card from "./Card";
 import { useSelector } from "react-redux";
+import OlvidateDeBuscar from "./OlvidateDeBuscar";
 
 const CardsContainer = () => {
-
-  const currentPublications = useSelector((state: any) => state.publication.publications);
+  const currentPublications = useSelector(
+    (state: any) => state.publication.publications
+  );
 
   return (
-    <div className="flex flex-wrap">
-      {currentPublications?.map((publ:any, index: number) => (
-        <Card
-          key={index}
-          property={publ.property}
-          propertyAddress={publ.propertyAddress}
-          propertyInformation={publ.propertyInformation}
-        />
-      ))}
+    <div className="w-[840px] h-[963px] grid grid-cols-3">
+      {currentPublications?.map((publ: any, index: number) => {
+        console.log("index: ", index);
+        if (index === 5) {
+          return <OlvidateDeBuscar />
+        } else {
+          return (
+            <Card
+              key={index}
+              property={publ.property}
+              propertyAddress={publ.propertyAddress}
+              propertyInformation={publ.propertyInformation}
+            />
+          );
+        }
+      })}
     </div>
   );
 };
