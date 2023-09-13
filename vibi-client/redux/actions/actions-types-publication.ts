daccc61926bef73dc180bdc5e60459a1ee625bd5
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-09-13 12:35:20 -0300

    Current Page in global state. Changes on pagination with filter.

--- vibi-client/redux/actions/actions-types-publication.ts
+++ vibi-client/redux/actions/actions-types-publication.ts
@@ -5,6 +5,7 @@ export const SET_PROPERTY_ADDRESS = "SET_PROPERTY_ADDRESS";
 export const SET_PROPERTY_DETAIL = "SET_PROPERTY_DETAIL";
 export const PUBLICATION = "PUBLICATION";
 export const CREATE_PUBLICATION = "CREATE_PUBLICATION";
+export const SET_CURRENTPAGE= "SET_CURRENTPAGE"
 
 
 export interface GetPublicationsAction {
@@ -15,3 +16,9 @@ export interface CreatePublication {
   type: 'CREATE_PUBLICATION',
   payload: {}
 }
+
+export interface SetCurrentPage {
+  type: 'SET_CURRENTPAGE',
+  payload: number
+}
+
