223bcf5d5eb0bd763d08fe45c9fc6d539f2629c5
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-01-30 18:38:32 -0300

    Disabled eslint in ButtonsVariants.tsx

--- vibi-client/components/commons/ButtonVariants.tsx
+++ vibi-client/components/commons/ButtonVariants.tsx
@@ -17,10 +17,10 @@ interface Props {
     href?: string,
     // eslint-disable-next-line no-unused-vars
     onClick?: (...args: any[]) => any
-
 }
+
 export default function ButtonVariants({ text, leftIcon, rightIcon, bgColor, txColor, border, brColor, width, href, onClick }: Props) {
-    const pathname = usePathname()
+    // const pathname = usePathname()
     if (onClick) {
         return (
             <button onClick={onClick} className={border ? `border ${brColor} ${bgColor}  rounded-[4px] px-6 py-3 flex  justify-center ${width} text-center` : ` ${bgColor}  rounded-[4px] px-6 py-3 flex justify-center ${width} text-center`}>
@@ -33,21 +33,8 @@ export default function ButtonVariants({ text, leftIcon, rightIcon, bgColor, txC
                 </section>
             </button>
         )
-    }; 
+    }
     return (
-
-        // <Link href={pathname + href}>
-        //     <div className={border ? `border ${brColor} ${bgColor} rounded-[4px] px-6 py-3 flex justify-center ${width} text-center` : ` ${bgColor} rounded-[4px] px-6 py-3 flex justify-center ${width} text-center`}>
-        //         <section className={`${txColor} ` + "text-[24px]"}>
-        //             {leftIcon}
-        //         </section>
-        //         <span className={`${rightIcon ? 'mr-2' : ''} ${leftIcon ? 'ml-2' : ''} ${txColor}`} >{text}</span>
-        //         <section className={`${txColor}` + "text-[24px]"}>
-        //             {rightIcon}
-        //         </section>
-        //     </div>
-        // </Link>
-
         <Link href={"" + href} className={border ? `border ${brColor} ${bgColor}  rounded-[4px] px-6 py-3 flex  justify-center ${width} text-center` : ` ${bgColor}  rounded-[4px] px-6 py-3 flex justify-center ${width} text-center`}>
             <section className={`${txColor} ` + "text-[24px]"}>
                 {leftIcon}
